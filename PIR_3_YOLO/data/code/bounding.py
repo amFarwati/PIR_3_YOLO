3269a0dfc12511cfb4102d3d5dc0f31ea9fdefbf
amFarwati <101759587+ElMagicarp@users.noreply.github.com>
2023-05-12 12:04:48 +0200

    Update bounding.py

--- PIR_3_YOLO/data/code/bounding.py
+++ PIR_3_YOLO/data/code/bounding.py
@@ -1,32 +1,112 @@
 import numpy as np
+import json
 import os
+import sys
 import cv2 as cv
+import glob
+import yoloTools as yolo
+from picsellia.types.enums import InferenceType
 
-file_path = '../BRATS/refined/labels/BRATS_001_e74.png'
+def polygonalisedContour(file_path):
+        
+        im = cv.imread(file_path)
+        assert im is not None, "file could not be read, check with os.path.exists()"
+        imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
+        ret, thresh = cv.threshold(imgray, 0, 255, 3)
 
-im = cv.imread(file_path)
-assert im is not None, "file could not be read, check with os.path.exists()"
-imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
-ret, thresh = cv.threshold(imgray, 0, 255, 3)
+        kernel = np.ones((5, 5), np.uint8)
+        thresh = cv.morphologyEx(thresh, cv.MORPH_CLOSE, kernel)
 
-kernel = np.ones((5, 5), np.uint8)
-thresh = cv.morphologyEx(thresh, cv.MORPH_CLOSE, kernel)
+        contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        for cnt in contours:
+                approx = cv.approxPolyDP(cnt, 0.01*cv.arcLength(cnt, True), True)
+                (x,y)=cnt[0,0]
+        
+        return (file_path,approx)
 
-for cnt in contours:
-   approx = cv.approxPolyDP(cnt, 0.01*cv.arcLength(cnt, True), True)
-   (x,y)=cnt[0,0]
 
-   if len(approx) >= 5:
-        img2 = np.zeros(im.shape,np.uint8)
-        print(approx)
-        img2 = cv.drawContours(img2, [approx], -1, (0,255,0), 1)
-        cv.imwrite("test poly.jpg",img2)
 
-#full_name = os.path.basename(file_path)
-#file_name = os.path.splitext(full_name)
+def trainDictionary(dico,file_path,contours):
 
-#f = open(file_name[0]+".txt", "x")
-#f.write(str(contours))
-#f.close()
+        try : 
+                id = len(dico["images"])
+        except :
+                id = 0
+
+        im = cv.imread(file_path)
+        height = im.shape[0]
+        width = im.shape[1]
+        poly  = []
+        file =""
+
+        for wrd in file_path.rsplit("/",12): 
+                if wrd == "labels":
+                        file = file+"/"+"train"
+                else:
+                        file = file+"/"+wrd
+
+        dico["images"].append(  {"id": id,
+                                "file_name":file,
+                                "height": height,
+                                "width": width
+                                })
+        
+        for segment in contours :
+                for point in segment:
+                        for coord in point:
+                                poly.append(coord) 
+
+        dico["annotations"].append(     {"id": id,
+                                        "image_id":id,
+                                        "segmentation": str(poly),
+                                        "category_id": 1,
+                                        "iscrowd": 0
+                                        })
+        return dico
+
+
+def convertToCOCO(dico):
+        json_Path = "declaration_labels.json"
+        with open(json_Path,"w") as f:
+                json_string = json.dump(dico,f)
+                return json_Path
+
+def convertToYolo(COCO_Path, img_Path):
+        converter = yolo.YOLOFormatter(
+                fpath = COCO_Path,
+                imdir = img_Path,
+                #steps = ["train"],
+                mode = InferenceType.SEGMENTATION
+        )
+
+        converter.convert()
+        yaml_fp = converter.generate_yaml(dpath = os.path.join(img_Path,'data.yaml'))
+
+        
+if __name__ == "__main__":
+
+        try:
+                dossier=sys.argv[1]
+        except:
+                print("ERROR: write the file's relative path")
+                exit(1)
+
+        absolute_path = os.path.dirname(__file__)
+        dir_abs_path = os.path.join(absolute_path, dossier)
+        file_path = []
+
+        for path in os.listdir(dir_abs_path):
+                file_path.append(os.path.join(dir_abs_path, path))
+
+        #print (file_path)
+
+        dico = {"info":{},"licenses":[],"images":[],"annotations":[],"categories":[{"id":1,"name":"tumeur"}]}
+
+        for img in file_path:
+                file, contour = polygonalisedContour(img)
+                dico = trainDictionary(dico,file,contour)
+
+        jsonCOCO_Path = convertToCOCO(dico)
+        convertToYolo(jsonCOCO_Path, dossier.rsplit('/', 2)[0]+'/')
+         
